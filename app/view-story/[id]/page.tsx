aac064e42b8ad03c6c6ef745bbe75232ec7ed9ed
Maor Atar <maoratar17@gmail.com>
2024-11-22 23:16:28 +0200

    Finished text to speech

--- app/view-story/[id]/page.tsx
+++ app/view-story/[id]/page.tsx
@@ -3,15 +3,18 @@
 import { db } from '@/config/db'
 import { StoryData } from '@/config/schema'
 import { eq } from 'drizzle-orm'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import HTMLFlipBook from 'react-pageflip';
 import BookCoverPage from '../_components/BookCoverPage'
 import StoryPages from '../_components/StoryPages'
 import LastPage from '../_components/LastPage'
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from "react-icons/io";
 
-function ViewStory({params}:any) {
+function ViewStory({ params }: any) {
   const [story, setStory] = useState<any>();
-  useEffect(()=>{
+  const bookRef = useRef();
+  const [count, setCount] = useState(0);
+  useEffect(() => {
     console.log(params.id)
     getStory();
   }, [])
@@ -23,35 +26,46 @@ function ViewStory({params}:any) {
   }
 
   return (
-    <div className='p-10 md:px-20 lg:px-40'>
-      <h2 className='font-bold text-4xl text-center p-10 bg-primary text-white'>{story?.output?.story_name}</h2>
-
-      {/* @ts-ignore */}
-      <HTMLFlipBook 
-      width={500} 
-      height={500}
-      showCover={true}
-      className='mt-10'
-      >
-        <div>
-          <BookCoverPage imageUrl={story?.coverImage} />         
-        </div>
-        {
-          [...Array(story?.output?.chapters?.length)].map((item, index) => (
-            <div className='bg-white p-10 border'>
+    <div className="p-10 md:px-20 lg:px-40 flex-col">
+      <h2 className="font-bold text-4xl text-center p-10 bg-primary text-white">
+        {story?.output?.story_name}
+      </h2>
+      <div className="relative">
+        {/* @ts-ignore */}
+        <HTMLFlipBook
+          width={500}
+          height={500}
+          showCover={true}
+          className="mt-10 mx-auto relative"
+          useMouseEvents={false}
+          ref={bookRef}
+        >
+          <div>
+            <BookCoverPage imageUrl={story?.coverImage} />
+          </div>
+          {[...Array(story?.output?.chapters?.length)].map((item, index) => (
+            <div key={index} className="bg-white p-10 border">
               <StoryPages storyChapter={story?.output?.chapters[index]} />
             </div>
-          ))
-        }
+          ))}
+        </HTMLFlipBook>
 
-        {/* <div>
-          <LastPage />
-        </div> */}
+        {count != 0 && <div className="absolute top-1/2 -translate-y-1/2 left-[165px]" onClick={() => {
+          bookRef.current.pageFlip().flipPrev();
+          setCount(count - 1);
+        }}>
+          <IoIosArrowDropleftCircle className='text-[40px] text-primary cursor-pointer' />
+        </div>}
 
-
-      </HTMLFlipBook>
-
+        {count != (story?.output?.chapters?.length - 1) && <div className="absolute top-1/2 -translate-y-1/2 right-[165px]" onClick={() => {
+          bookRef.current.pageFlip().flipNext();
+          setCount(count + 1);
+        }}>
+          <IoIosArrowDroprightCircle className='text-[40px] text-primary cursor-pointer' />
+        </div>}
+      </div>
     </div>
+
   )
 }
 
--- app/view-story/_components/StoryPages.tsx
+++ app/view-story/_components/StoryPages.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { MdOutlinePlayCircleFilled } from "react-icons/md";
 
 function StoryPages({storyChapter}:any) {
+const playSpeech = (text:string) => {
+  const synth = window.speechSynthesis;
+  const voices = synth.getVoices();
+  const hebrewVoice = voices.find(voice => voice.lang === "he-IL");
+
+  if (hebrewVoice) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = hebrewVoice;
+    synth.speak(utterance);
+  }
+};
+
+
   return (
     <div>
-        <h2 className='text-2xl text-right fontbold text-primary'>{storyChapter?.chapter_title}</h2>
+        <h2 className='text-2xl text-right fontbold text-primary flex justify-between'>{storyChapter?.chapter_title}
+            <span className='text-3xl cursor-pointer' onClick={()=>playSpeech(storyChapter?.text)}><MdOutlinePlayCircleFilled /></span>
+        </h2>
         <p dir="rtl" className='text-xl text-black p-10 mt-3 rounded-lg bg-slate-100'>
             {storyChapter?.text}
         </p>
